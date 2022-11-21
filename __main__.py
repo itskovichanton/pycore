69dd3c4ca6b33a3ee972e0cdff98c94885824ccc
anton <itskovichae@gmail.com>
2022-11-21 09:02:38 +0300

    correct package structure

--- __main__.py
+++
@@ -1,6 +0,0 @@
-from alerts import AlertService, AlertServiceImpl
-from config import ConfigService, ConfigServiceImpl
-from email_service import EmailService, EmailServiceImpl
-from fr import FRConfig, FRService, FRServiceImpl
-from di import CoreModule
-from utils import trim_string
