baa88f72f7e6a260122c1b21a76ffa11a85c5fa4
anton <itskovichae@gmail.com>
2022-12-13 11:37:02 +0300

    logger encoding added

--- src/mybootstrap_core_itskovichanton/logger.py
+++ src/mybootstrap_core_itskovichanton/logger.py
@@ -6,12 +6,13 @@ from logging.handlers import TimedRotatingFileHandler
 from typing import Protocol
 
 from pythonjsonlogger import jsonlogger
-from src.mybootstrap_core_itskovichanton.config import ConfigService
 from src.mybootstrap_ioc_itskovichanton.ioc import bean
+
+from src.mybootstrap_core_itskovichanton.config import ConfigService
 
 
 class LoggerService(Protocol):
-    def get_file_logger(self, name: str) -> Logger:
+    def get_file_logger(self, name: str, encoding: str = "utf-8") -> Logger:
         pass
 
 
@@ -27,7 +28,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
 class LoggerServiceImpl(LoggerService):
     config_service: ConfigService
 
-    def get_file_logger(self, name: str) -> Logger:
+    def get_file_logger(self, name: str, encoding: str = "utf-8") -> Logger:
         r = logging.getLogger(name)
         if hasattr(r, "inited"):
             return r
@@ -36,6 +37,7 @@ class LoggerServiceImpl(LoggerService):
         settings = self.config_service.config.settings
         r.setLevel(settings.get(logger_settings_prefix + ".level", logging.INFO))
         log_handler = TimedRotatingFileHandler(
+            encoding=encoding,
             filename=f"{os.path.join(self.config_service.dir('logs'), name)}-{self.config_service.app_name()}.txt",
             when=settings.get(logger_settings_prefix + ".when", "midnight"),
             backupCount=settings.get(logger_settings_prefix + ".backup_count", 10))
